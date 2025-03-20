528bb4d4546b9aad1b46f83efed98b223163e88e
Dmitry Tumalanov <tdmitry@fibex.su>
2025-03-20 14:29:41 +0300

    Refactor processor

--- internal/app/handlers/handler.go
+++ internal/app/handlers/handler.go
@@ -1,9 +1,15 @@
 package handlers
 
 import (
+	"github.com/Extremal37/go-http-api-sample/internal/app/models"
 	"github.com/Extremal37/go-http-api-sample/internal/app/processor"
 	"go.uber.org/zap"
 )
+
+type Processor interface {
+	AddContact(contact models.Contact)
+	GetContacts() []models.Contact
+}
 
 type Handler struct {
 	p   *processor.Processor
