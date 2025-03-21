becce3e23b901ab76fee7e077319243979da6835
Dmitry Tumalanov <tdmitry@fibex.su>
2025-03-21 11:59:49 +0300

    Add postgres to config

--- internal/cfg/config.go
+++ internal/cfg/config.go
@@ -12,8 +12,11 @@ const (
 	configType = "yaml"
 )
 const (
-	appLogging    = "debug"
-	appListenPort = ":8080"
+	appLogging       = "debug"
+	appListenPort    = ":8080"
+	postgresHost     = "localhost"
+	postgresPort     = 5432
+	postgresDatabase = "postgres"
 )
 
 type App struct {
@@ -21,8 +24,17 @@ type App struct {
 	Address string `yaml:"address"`
 }
 
+type Postgres struct {
+	Host     string `yaml:"host"`
+	Port     uint   `yaml:"port"`
+	Database string `yaml:"database"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
 type Configuration struct {
-	App App `yaml:"app"`
+	App      App      `yaml:"app"`
+	Postgres Postgres `yaml:"postgres"`
 }
 
 // LoadAndStoreConfig - Load configuration from file.
@@ -35,6 +47,9 @@ func LoadAndStoreConfig() (*Configuration, error) {
 
 	v.SetDefault("App.Logging", appLogging)
 	v.SetDefault("App.ListenPort", appListenPort)
+	v.SetDefault("Postgres.host", postgresHost)
+	v.SetDefault("Postgres.Port", postgresPort)
+	v.SetDefault("Postgres.Database", postgresDatabase)
 
 	cfg := &Configuration{}
 
