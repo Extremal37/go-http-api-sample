31fe7db43ed7f9ba78117e78cdf411d7e2667681
Dmitry Tumalanov <tdmitry@fibex.su>
2025-03-25 15:36:32 +0300

    Refactor errors wrapping in processor

--- internal/app/processor/contact.go
+++ internal/app/processor/contact.go
@@ -2,15 +2,24 @@ package processor
 
 import (
 	"context"
+	"fmt"
 	"github.com/Extremal37/go-http-api-sample/internal/app/models"
 )
 
 // AddContact Saving new contact to the storage
 func (p *Processor) AddContact(ctx context.Context, contact models.Contact) error {
-	return p.storage.AddContact(ctx, contact)
+	err := p.storage.AddContact(ctx, contact)
+	if err != nil {
+		return fmt.Errorf("failed to add contact to storage: %w", err)
+	}
+	return nil
 }
 
 // GetContacts Return all contacts from the storage
 func (p *Processor) GetContacts(ctx context.Context) ([]models.Contact, error) {
-	return p.storage.GetContacts(ctx)
+	contacts, err := p.storage.GetContacts(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get contacts from storage: %w", err)
+	}
+	return contacts, nil
 }
